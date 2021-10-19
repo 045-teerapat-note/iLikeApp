a6ae270e8c696c2b9cc51faf13e06b1e6e6fc6a8
330note111 <note33110@gmail.com>
2021-10-19 11:00:06 +0700

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,221 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createStackNavigator} from '@react-navigation/stack';
+import { StatusBar } from 'expo-status-bar';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, Button, ImageBackground } from 'react-native';
 
-export default function App() {
+const Tab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
+const Stack = createStackNavigator();
+
+function HomeScreen() {
+return (
+    <Tab.Navigator
+    
+        screenOptions={({ route }) => ({
+          tabBarActiveTintColor: '#00FFFF',
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+           
+            if (route.name === 'หน้าแรก') {
+              iconName = focused ? 'md-laptop-sharp' : 'md-laptop-outline';
+            } else if (route.name === 'เกี่ยวกับเรา') {
+              iconName = focused ? 'ios-logo-octocat' : 'ios-logo-reddit';
+            } else if (route.name === 'บริการของเรา') {
+              iconName = focused ? 'ios-man' : 'ios-man-outline';
+            } else if (route.name === 'ติดต่อเรา') {
+              iconName = focused ? 'ios-phone-portrait' : 'ios-phone-portrait-outline';
+            } else if (route.name === 'ลงทะเบียน') {
+              iconName = focused ? 'people' : 'people';
+            }
+            
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={"green"} />;
+          },
+          //tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+
+        })}
+
+    >
+          <Tab.Screen name="หน้าแรก" component={TabA} options={{ headerShown: false }}/>
+          <Tab.Screen name="เกี่ยวกับเรา" component={TabB} options={{ headerShown: false }}/>
+          <Tab.Screen name="บริการของเรา" component={TabC} options={{ headerShown: false }}/>
+          <Tab.Screen name="ติดต่อเรา" component={TabD} options={{ headerShown: false }}/>
+          <Tab.Screen name="ลงทะเบียน" component={TabE} options={{ headerShown: false }}/>
+    </Tab.Navigator>
+);
+}
+
+function TabA({navigation}) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center',  alignItems: 'center' }}>
+      <Text>
+      ยินดีต้อนรับสู่ หน้าแรก
+      </Text>
     </View>
   );
+}
+
+
+function TabB({navigation}) {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center',  alignItems: 'center' }}>
+      <Text>
+      ยินดีต้อนรับสู่ เกี่ยวกับเรา
+      </Text>
+    </View>
+  );
+}
+
+function TabC({navigation}) {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center',  alignItems: 'center' }}>
+      <Text>
+      ยินดีต้อนรับสู่ บริการของเรา
+      </Text>
+    </View>
+  );
+}
+
+function TabD({navigation}) {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center',  alignItems: 'center' }}>
+      <Text>
+      ยินดีต้อนรับสู่ ติดต่อเรา
+      </Text>
+    </View>
+  );
+}
+
+function TabE({navigation}) {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center',  alignItems: 'center' }}>
+      <Text>
+      ยินดีต้อนรับสู่ ลงทะเบียน
+      </Text>
+    </View>
+  );
+}
+
+function login({navigation}) {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center',  alignItems: 'center' }}>
+    <Text>ลงทะเบียนเข้าสู่ระบบ</Text>
+    <Text> </Text>
+          <TextInput
+      style={styles.input}
+      onChangeText={text => onChangeText(text)}
+      value={""}
+      placeholder={'Username'}
+    />
+
+    <TextInput
+      style={styles.input}
+      onChangeText={text => onChangeText(text)}
+      value={""}
+      placeholder={'Password'}
+    />
+
+<Button
+    title="ลงทะเบียน"
+    style = {styles.Btn}
+    ></Button>        
+    </View>
+  );
+}
+
+
+function subscribe({navigation}) {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center',  alignItems: 'center' }}>
+<Text>ลงทะเบียน</Text>
+    <Text> </Text>
+          <TextInput
+      style={styles.input}
+      onChangeText={text => onChangeText(text)}
+      value={""}
+      placeholder={'ชื่อ-นามสกุล'}
+    />
+
+    <TextInput
+      style={styles.input}
+      onChangeText={text => onChangeText(text)}
+      value={""}
+      placeholder={'Username'}
+    />
+
+<TextInput
+      style={styles.input}
+      onChangeText={text => onChangeText(text)}
+      value={""}
+      placeholder={'Password'}
+    />
+    <TextInput
+      style={styles.input}
+      onChangeText={text => onChangeText(text)}
+      value={""}
+      placeholder={'Confiem Password'}
+    />
+    <TextInput
+      style={styles.input}
+      onChangeText={text => onChangeText(text)}
+      value={""}
+      placeholder={'ที่อยู่'}
+    />
+    <TextInput
+      style={styles.input}
+      onChangeText={text => onChangeText(text)}
+      value={""}
+      placeholder={'เบอร์โทร'}
+    />
+      <Button
+    title="ลงทะเบียน"
+    style = {styles.Btn}
+    ></Button>        
+    </View>
+  );
+}
+
+export default function App() {
+return(
+
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="หน้าแรก" component={HomeScreen}/>
+        <Drawer.Screen name="เกี่ยวกับเรา" component={""} />
+        <Drawer.Screen name="บริการของเรา" component={""} />
+        <Drawer.Screen name="ติดต่อเรา" component={""} />
+        <Drawer.Screen name="เข้าสู่ระบบ" component={login} />
+        <Drawer.Screen name="สมัครสมาชิก" component={subscribe} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+
+  /*
+    <NavigationContainer>
+    <HomeScreen />
+  </NavigationContainer>
+  */
+);
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+  },
+  input: {
+    width: 200,
+    height: 44,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: 'black',
+    marginBottom: 10,
+    borderRadius: 10,
   },
 });
